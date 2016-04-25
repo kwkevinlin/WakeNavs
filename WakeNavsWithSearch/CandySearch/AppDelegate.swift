0aa542a9d13ee2aeb2971dafd44ba3211b34337d
kwkevinlin <kwkevinlin@gmail.com>
2016-04-25 00:17:52 -0400

    Added Google API fetch, updated gitignore

--- WakeNavsWithSearch/CandySearch/AppDelegate.swift
+++ WakeNavsWithSearch/CandySearch/AppDelegate.swift
@@ -1,40 +1,61 @@
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
-  
-  var window: UIWindow?
-  
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    // Override point for customization after application launch.
-    let splitViewController = window!.rootViewController as! UISplitViewController
-    let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
-    navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
-    splitViewController.delegate = self
     
-    UISearchBar.appearance().barTintColor = UIColor.candyGreen()
-    UISearchBar.appearance().tintColor = UIColor.whiteColor()
-    UITextField.appearanceWhenContainedInInstancesOfClasses([UISearchBar.self]).tintColor = UIColor.candyGreen()
-    return true
-  }
-  
-  // MARK: - Split view
-  func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
-    guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-    guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-    if topAsDetailController.detailBuilding == nil {
-      // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-      return true
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        //Search bar setup
+        let splitViewController = window!.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
+        splitViewController.delegate = self
+        
+        UISearchBar.appearance().barTintColor = UIColor.candyGreen()
+        UISearchBar.appearance().tintColor = UIColor.whiteColor()
+        UITextField.appearanceWhenContainedInInstancesOfClasses([UISearchBar.self]).tintColor = UIColor.candyGreen()
+        
+        
+        // Retrieve API key from keys.plist
+        var keyList: NSDictionary?
+        
+        if let path = NSBundle.mainBundle().pathForResource("keys", ofType: "plist") {
+            keyList = NSDictionary(contentsOfFile: path)
+        }
+        if let _ = keyList {
+            let GoogleAPI = keyList?["GoogleAPI"] as? String
+            
+            /* Google Dev Account (enabled):
+            Google Maps SDK (iOS)
+            Google Directions API
+            */
+            GMSServices.provideAPIKey(GoogleAPI!)
+        }
+        
+        return true
     }
-    return false
-  }
-  
+    
+    // MARK: - Split view
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        if topAsDetailController.detailBuilding == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
+    }
+    
 }
 
 extension UIColor {
-  static func candyGreen() -> UIColor {
-    return UIColor(red: 158.0/255.0, green: 126.0/255.0, blue: 56.0/255.0, alpha: 1.0)
-  }
+    static func candyGreen() -> UIColor {
+        return UIColor(red: 158.0/255.0, green: 126.0/255.0, blue: 56.0/255.0, alpha: 1.0)
+    }
 }
 
