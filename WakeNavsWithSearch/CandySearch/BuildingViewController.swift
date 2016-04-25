ab4674470e3f4d21ad3e199a0d8bbb34d29f2e84
kwkevinlin <kwkevinlin@gmail.com>
2016-04-25 00:10:31 -0400

    Created BuildingViewController

---
+++ WakeNavsWithSearch/CandySearch/BuildingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BuildingViewController.swift
+//  WakeNavs
+//
+//  Created by 1834 Software on 4/25/16.
+//  Copyright © 2016 Peartree Developers. All rights reserved.
+//
+
+import UIKit
+
+class BuildingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+
+}
