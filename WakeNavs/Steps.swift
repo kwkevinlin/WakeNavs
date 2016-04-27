a0ad1dd90b39396cb3cb51bd8d0edeb6a2ad786e
kwkevinlin <kwkevinlin@gmail.com>
2016-04-27 01:29:18 -0400

    Removed distance and duration

--- WakeNavs/Steps.swift
+++ WakeNavs/Steps.swift
@@ -10,16 +10,11 @@ import Foundation
 import CoreLocation
 
 class Steps {
-    var duration: Int = 0
-    var distance: Int = 0
     var coordinates: CLLocationCoordinate2D
     var encodedPoly: String
     var instructions: String = ""
     
-    init(dur: Int, dist: Int, coor: CLLocationCoordinate2D, poly: String, inst: String) {
-        self.duration = dur
-        self.distance = dist
-        self.coordinates = coor
+    init(coor: CLLocationCoordinate2D, poly: String, inst: String) {        self.coordinates = coor
         self.encodedPoly = poly
         self.instructions = inst
     }
